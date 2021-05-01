84ec45cb373263ac9347960d287bd41e0885e375
Drew Brumbach <Drew_Brumbach1@baylor.edu>
2021-05-01 00:51:24 -0500

    Templates for pizzas and toppings

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Pizza, Topping
+from django.http import Http404
 
 # Create your views here.
 
@@ -6,3 +8,19 @@ from django.shortcuts import render
 def index(request):
     """the home page for pizzeria."""
     return render(request, "pizzas/index.html")
+
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by("text")
+
+    context = {"pizzas": pizzas}
+    return render(request, "pizzas/pizzas.html", context)
+
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by("-text")
+
+    context = {"pizza": pizza, "toppings": toppings}
+
+    return render(request, "pizzas/pizza.html", context)
